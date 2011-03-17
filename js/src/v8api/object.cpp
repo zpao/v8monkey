d05aa640f2ca7d6bdb499984eabd386517b5cd19
Shawn Wilsher <me@shawnwilsher.com>
2011-03-16 19:43:56 -0700

    Fix style

--- js/src/v8api/object.cpp
+++ js/src/v8api/object.cpp
@@ -3,32 +3,42 @@
 namespace v8 {
 using namespace internal;
 
-bool Object::Set(Handle<Value> key, Handle<Value> value, PropertyAttribute attribs) {
+bool
+Object::Set(Handle<Value> key,
+            Handle<Value> value,
+            PropertyAttribute attribs) {
   // TODO: use String::Value
   String::AsciiValue k(key);
   jsval v = value->native();
 
-  if (!JS_SetProperty(*cx(), *this, *k, &v))
+  if (!JS_SetProperty(*cx(), *this, *k, &v)) {
     return false;
+  }
+
   uintN js_attribs = 0;
-  if (attribs & ReadOnly)
+  if (attribs & ReadOnly) {
     js_attribs |= JSPROP_READONLY;
-  if (!(attribs & DontEnum))
+  }
+  if (!(attribs & DontEnum)) {
     js_attribs |= JSPROP_ENUMERATE;
-  if (attribs & DontDelete)
+  }
+  if (attribs & DontDelete) {
     js_attribs |= JSPROP_PERMANENT;
+  }
 
   JSBool wasFound;
   return JS_SetPropertyAttributes(*cx(), *this, *k, js_attribs, &wasFound);
 }
 
-Local<Value> Object::Get(Handle<Value> key) {
+Local<Value>
+Object::Get(Handle<Value> key) {
   // TODO: use String::Value
   String::AsciiValue k(key);
   Value v(JSVAL_VOID);
 
-  if (JS_GetProperty(*cx(), *this, *k, &v.native()))
+  if (JS_GetProperty(*cx(), *this, *k, &v.native())) {
     return Local<Value>::New(&v);
+  }
   return Local<Value>();
 }
 
@@ -37,11 +47,14 @@ Object::Object(JSObject *obj) :
 {
 }
 
-Local<Object> Object::New() {
+Local<Object>
+Object::New() {
   JSObject *obj = JS_NewObject(*cx(), NULL, NULL, NULL);
-  if (!obj)
+  if (!obj) {
     return Local<Object>();
+  }
   Object o(obj);
   return Local<Object>::New(&o);
 }
-}
+
+} /* namespace v8 */

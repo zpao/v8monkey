3ae347a1e7e731cf4c60db073a39e93c38ae3e4e
Shawn Wilsher <me@shawnwilsher.com>
2011-04-25 12:36:38 -0700

    Copy and disable test_DefinePropertyOnDefineGetterSetter (same failure as Issue #65)

--- js/src/v8api/test/test_api.cpp
+++ js/src/v8api/test/test_api.cpp
@@ -1643,7 +1643,51 @@ test_DefinePropertyOnAPIAccessor()
 // from test-api.cc:2840
 void
 test_DefinePropertyOnDefineGetterSetter()
-{ }
+{
+  v8::HandleScope scope;
+  Local<ObjectTemplate> templ = ObjectTemplate::New();
+  templ->SetAccessor(v8_str("x"), GetXValue, NULL, v8_str("donut"));
+  LocalContext context;
+  context->Global()->Set(v8_str("obj"), templ->NewInstance());
+
+  Local<Script> script_desc = Script::Compile(v8_str("var prop ="
+                                    "Object.getOwnPropertyDescriptor( "
+                                    "obj, 'x');"
+                                    "prop.configurable;"));
+  Local<Value> result = script_desc->Run();
+  CHECK_EQ(result->BooleanValue(), true);
+
+  Local<Script> script_define =
+    Script::Compile(v8_str("var desc = {get: function(){return 42; },"
+                           "            configurable: true };"
+                           "Object.defineProperty(obj, 'x', desc);"
+                           "obj.x"));
+  result = script_define->Run();
+  CHECK_EQ(result, v8_num(42));
+
+
+  result = script_desc->Run();
+  CHECK_EQ(result->BooleanValue(), true);
+
+
+  script_define =
+    Script::Compile(v8_str("var desc = {get: function(){return 43; },"
+                           "            configurable: false };"
+                           "Object.defineProperty(obj, 'x', desc);"
+                           "obj.x"));
+  result = script_define->Run();
+  CHECK_EQ(result, v8_num(43));
+  result = script_desc->Run();
+
+  CHECK_EQ(result->BooleanValue(), false);
+
+  v8::TryCatch try_catch;
+  result = script_define->Run();
+  CHECK(try_catch.HasCaught());
+  String::AsciiValue exception_value(try_catch.Exception());
+  CHECK_EQ(*exception_value,
+           "TypeError: Cannot redefine property: defineProperty");
+}
 
 // from test-api.cc:2893
 void
@@ -4118,7 +4162,7 @@ Test gTests[] = {
   DISABLED_TEST(test_MultiRun, 43),
   TEST(test_SimplePropertyRead),
   DISABLED_TEST(test_DefinePropertyOnAPIAccessor, 65),
-  UNIMPLEMENTED_TEST(test_DefinePropertyOnDefineGetterSetter),
+  DISABLED_TEST(test_DefinePropertyOnDefineGetterSetter, 65),
   DISABLED_TEST(test_DefineAPIAccessorOnObject, 66),
   UNIMPLEMENTED_TEST(test_DontDeleteAPIAccessorsCannotBeOverriden),
   TEST(test_ElementAPIAccessor),

fdd23a157247500d99f29e6b1d08e32615de54ab
Philipp von Weitershausen <philipp@weitershausen.de>
2011-05-10 21:52:23 -0500

    Some more tests for Value methods that aren't tested in test_api

--- js/src/v8api/test/test_value.cpp
+++ js/src/v8api/test/test_value.cpp
@@ -10,8 +10,13 @@ static inline Local<Value> CompileRun(const char* source) {
 ////////////////////////////////////////////////////////////////////////////////
 //// Tests
 
+/*
+ * Most of Value's methods should already be (either implicitly or
+ * explicitly) tested in test_api. The one's that aren't are covered here.
+ */
+
 void
-test_ToInteger()
+test_IntegerConversion()
 {
   HandleScope handle_scope;
   Persistent<Context> context = Context::New();
@@ -19,39 +24,96 @@ test_ToInteger()
 
   // Convert string to Integer.
   Handle<Value> result = CompileRun("'123'");
-  Local<Integer> integer = result->ToInteger();
-  do_check_eq(integer->Value(), 123);
+  do_check_eq(result->IntegerValue(), 123);
+  do_check_eq(result->ToInteger()->Value(), 123);
 
   // Convert integer number to Integer.
   result = CompileRun("123");
-  integer = result->ToInteger();
-  do_check_eq(integer->Value(), 123);
+  do_check_eq(result->IntegerValue(), 123);
+  do_check_eq(result->ToInteger()->Value(), 123);
 
   // Convert decimal number to Integer.
   result = CompileRun("123.45");
-  integer = result->ToInteger();
-  do_check_eq(integer->Value(), 123);
+  do_check_eq(result->IntegerValue(), 123);
+  do_check_eq(result->ToInteger()->Value(), 123);
 
   // Convert negative number to Integer.
   result = CompileRun("-123");
-  integer = result->ToInteger();
-  do_check_eq(integer->Value(), -123);
+  do_check_eq(result->IntegerValue(), -123);
+  do_check_eq(result->ToInteger()->Value(), -123);
 
 /* TODO: we don't support 64 bit numbers yet
 
   // Convert huge number to Integer.
   result = CompileRun("0x7fffffffffffffff");
-  integer = result->ToInteger();
-  do_check_eq(integer->Value(), (JSInt64)0x7fffffffffffffff);
+  do_check_eq(result->IntegerValue(), (JSInt64)0x7fffffffffffffff);
+  do_check_eq(result->ToInteger()->Value(), (JSInt64)0x7fffffffffffffff);
 */
+
+  // Convert NaN to Integer.
+  result = CompileRun("NaN");
+  do_check_eq(result->IntegerValue(), 0);
+  do_check_eq(result->ToInteger()->Value(), 0);
 }
 
+void
+test_BooleanConversion()
+{
+  HandleScope handle_scope;
+  Persistent<Context> context = Context::New();
+  Context::Scope context_scope(context);
+
+  // Convert an a truthy value, e.g. a string, to a boolean.
+  Handle<Value> result = CompileRun("'123'");
+  do_check_true(result->BooleanValue());
+  do_check_true(result->ToBoolean()->Value());
+
+  // Convert an a truthy value, e.g. a string, to a boolean.
+  result = CompileRun("0");
+  do_check_false(result->BooleanValue());
+  do_check_false(result->ToBoolean()->Value());
+}
+
+void
+test_IsFunction()
+{
+  HandleScope handle_scope;
+  Persistent<Context> context = Context::New();
+  Context::Scope context_scope(context);
+
+  // Check something that isn't a function, e.g. a string.
+  Handle<Value> result = CompileRun("'123'");
+  do_check_false(result->IsFunction());
+
+  // Check function.
+  result = CompileRun("function () {}");
+  do_check_true(result->IsFunction());
+}
+
+void
+test_IsArray()
+{
+  HandleScope handle_scope;
+  Persistent<Context> context = Context::New();
+  Context::Scope context_scope(context);
+
+  // Check something that isn't a function, e.g. a string.
+  Handle<Value> result = CompileRun("{}");
+  do_check_false(result->IsArray());
+
+  // Check function.
+  result = CompileRun("[]");
+  do_check_true(result->IsArray());
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 //// Test Harness
 
 Test gTests[] = {
-  TEST(test_ToInteger),
+  TEST(test_IntegerConversion),
+  TEST(test_BooleanConversion),
+  TEST(test_IsFunction),
+  TEST(test_IsArray),
 };
 
 const char* file = __FILE__;
